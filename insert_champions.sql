b9a3c40e3b6c7767d3470985064aaa91e4d56042
Wesley Ryder <wr140815@ohio.edu>
2019-11-12 14:37:59 -0500

    Make a forloop?... nahhh we cooking with grease

--- insert_champions.sql
+++ insert_champions.sql
@@ -1,65 +1,63 @@
-INSERT INTO champion 
-VALUES
-('Vayne', 'ADC', 'Night Hunter', 'Tumble', 'Silver Bolts', 'Condemn', 'Final Hour', 'Hyper Carry'),
-('Aatrox', 'TOP', 'Deathbringer', 'Darkin Dlade', 'Infernal Chains', 'Umbral Dash', 'World Ender', 'Bruiser'),
-('Ahri', 'MID', 'Vastayan Grace', 'Orb of Deception', 'Fox-Fire', 'Charm', 'Spirit Rush', 'Mage'),
-('Akali', 'MID', 'Assassin Mark', 'Five Point Strike', 'Twilight Shroud', 'Shuriken Flip', 'Perfect Execution', 'Assassin'),
-('Alistar', 'SUP', 'Triumphant Roar', 'Pulverize', 'Headbutt', 'Trample', 'Unbreakable Will', 'Tank'),
-('Ashe', 'ADC', 'Frost Shot', 'Ramper Focus', 'Volley', 'Hawkshot', 'Enchanted Arrow', 'Ranged Carry'),
-('Azir', 'MID', 'Shurima Legacy', 'Conquering Sands', 'Arise', 'Shifting Sands', 'Emperor Divide', 'Mage'),
-('Bard', 'SUP', 'Traveler Call', 'Cosmic Binding', 'Caretaker Shrine', 'Magical Journey', 'Tempered Fate', 'Mage'),
-('Blitz', 'SUP', 'Mana Barrier', 'Rocket Grab', 'Overdrive', 'Power Fist', 'Static Field', 'Bruiser'),
-('Braum', 'SUP', 'Concussive Blows', 'Winter Bite', 'Stand Behind Me', 'Unbreakable', 'Glacial Fissure', 'Tank'),
-('Caitlyn', 'ADC', 'Headshot', 'Peacemaker', 'Yordle Snap Trap', '90 Caliber net', 'Ace in the Hole', 'Ranged Carry'),
-('Camile', 'TOP', 'Adaptive Defense', 'Precision Protocol', 'Tatical Sweep', 'Hookshot', 'Hextech Ultimatum', 'Bruiser'),
-('Cassiopeia', 'MID', 'Serpentine Grace', 'Noxious Blast', 'Miasma', 'Twin Fang', 'Petrifying Gaze', 'Mage'),
-('Diana', 'MID', 'Moonsilver Blade', 'Crescent Strike', 'Pale Cascade', 'Moonfall', 'Lunar Rush', 'Assassin'),
-('Dr. Mundo', 'TOP', 'Adrenaline Rush', 'Infected Cleaver', 'Burning Agony', 'Masochism', 'Sadism', 'Tank'),
-('Draven', 'ADC', 'League of Draven', 'Spinning Axe', 'Blood Rush', 'Stand Aside', 'Whirling Death', 'Ranged Carry'),
-('Ekko', 'JNG', 'Z-Drive', 'Timewinder', 'Parallel Convergence', 'Phase Dive', 'Chronobreak', 'Mage'),
-('Elise', 'JNG', 'Spider Queen', 'Neurotoxin', 'Volatile Spiderling', 'Cacoon', 'Spider Form', 'Assassin'),
-('Ezreal', 'ADC', 'Rising Spell Force', 'Mystic Shot', 'Essence Flux', 'Arcane Shift', 'Trueshot Barrage', 'Ranged Carry'),
-('Fiora', 'TOP', 'Duelist Dance', 'Lunge', 'Riposte', 'Bladework', 'Grand Challenge', 'Bruiser'),
-('Fizz', 'MID', 'Nimble Fighter', 'Urchin Strike', 'Seastone Trident', 'Trickster', 'Chum the Waters', 'Assassin'),
-('Galio', 'MID', 'Colossal Smash', 'Winds of War', 'Shield of Durand', 'Justice Punch', 'Hero Entrance', 'Tank'),
-('Gangplank', 'TOP', 'Trial by Fire', 'Parrley', 'Remove Scurvy', 'Powder Keg', 'Cannon Barrage', 'Fighter'),
-('Garen', 'TOP', 'Perseverance', 'Decisive Strike', 'Courage', 'Judgment', 'Demacian Justice', 'Bruiser'),
-('Gragas', 'JNG', 'Happy Hour', 'Barrel Roll', 'Druken Rage', 'Body Slam', 'Explosive Cask', 'Bruiser'),
-('Graves', 'JNG', 'New Destiny', 'End of the Line', 'Smoke Screen', 'Quickdraw', 'Collateral Damage', 'Fighter'),
-('Hecarim', 'JNG', 'Warpath', 'Rampage', 'Spirit of Dread', 'Devastating Charge', 'Onslaught of Shadows', 'Bruiser'),
-('Irelia', 'TOP', 'Ionian Fervor', 'Bladesurge', 'Defiant Dance', 'Flawless Duet', 'Vanguard Edge', 'Fighter'),
-('Ivern', 'JNG', 'Friend the Forest', 'Rootcaller', 'Brushmaker', 'Triggerseed', 'Daisy!', 'Mage'),
-('Janna', 'SUP', 'Tailwind', 'Howling Gale', 'Zephyr', 'Eye Of The Storm', 'Monsoon', 'Mage'),
-('Jarvan IV', 'JNG', 'Martial Cadence', 'Dragon Strike', 'Golden Aegis', 'Demacian Standard', 'Cataclysm', 'Bruiser'),
-('Jax', 'TOP', 'Relentless Atk', 'Leap Strike', 'Empower', 'Counter Strike', 'Grand Might', 'Bruiser'),
-('Jayce', 'MID', 'Hextech Capacitor', 'Shock Blast', 'Hyper Charge', 'Acceleration Gate', 'Mecury Cannon', 'Fighter'),
-('Jhin', 'ADC', 'Whisper', 'Dancing Grenade', 'Deadly Flourish', 'Captive Audience', 'Curtain Call', 'Ranged Carry'),
-('Jinx', 'ADC', 'Get Excited!', 'Switcheroo!', 'Zap!', 'Flame Chompers!', 'Super Mega Rocket!', 'Hyper Carry'),
-('Kaisa', 'ADC', 'Second Skin', 'Icathian Rain', 'Void Seeker', 'Supercharge', 'Killer Instinct', 'Ranged Carry'),
-('Kalista', 'ADC', 'Martial Poise', 'Pierce', 'Sentinel', 'Rend', 'Fate Call', 'Ranged Carry'),
-('Karma', 'SUP', 'Gathering Fire', 'Inner Flame', 'Focused Resolve', 'Inspire', 'Mantra', 'Mage'),
-('Kayn', 'JNG', 'Darkin Scythe', 'Reaping Slash', 'Blade Reach', 'Shadow Step', 'Umbral Trespass', 'Bruiser'),
-('Khazix', 'JNG', 'Unseen Threat', 'Taste Their Fear', 'Void Spike', 'Leap', 'Void Assult', 'Assassin'),
-('Kled', 'TOP', 'Skaarl, the Lizard', 'Bear Trap', 'Violent Tendencies', 'Jousting', 'Chaaaaarge!!!', 'Bruiser'),
-('Lee Sin', 'JNG', 'Flurry', 'Resonating Strike', 'Safeguard', 'Tempest', 'Dragon Rage', 'Fighter'),
-('Lucian', 'ADC', 'Lightslinger', 'Piercing Light', 'Ardent Blast', 'Relentless Pursuit', 'The Culling', 'Ranged Carry'),
-('Lulu', 'SUP', 'Pix Faerie', 'Glitterlance', 'Whimsy', 'Help, Pix!', 'Wild Growth', 'Mage'),
-('Nami', 'SUP', 'Surging Tides', 'Aqua Prison', 'Ebb and Flow', 'Tidecaller Blessing', 'Tidal Wave', 'Mage'),
-('Nautilus', 'SUP', 'Staggering Blow', 'Dredge Line', 'Titan Wrath', 'Riptide', 'Depth Charge', 'Tank'),
-('Nidalee', 'JNG', 'Prowl', 'Javelin Toss', 'Bushwhack', 'Swipe', 'Transform', 'Assassin'),
-('Olaf', 'JNG', 'Berserker Rage', 'Undertow', 'Vicious Strikes', 'Reckless Swing', 'Ragnarok', 'Bruiser'),
-('Oriana', 'MID', 'Clockwork Windup', 'CMD:Attack', 'CMD:Dissonance', 'CMD:Protect', 'CMD:Shockwave', 'Mage'),
-('Ornn', 'TOP', 'Living Forge', 'Volcanic Rupture', 'Bellows Breath', 'Searing Charge', 'Call of Forge God', 'Tank'),
-('Reksai', 'JNG', 'Fury of the XerSai', 'Prey Seeker', 'Burrow', 'Tunnel', 'Void Rush', 'Bruiser'),
-('Rengar', 'JNG', 'Unseen Predator', 'Savager', 'Battle Roar', 'Bola Strike', 'Thrill of the Hunt', 'Assassin'),
-('Shen', 'TOP', 'Ki Barrier', 'Twilight Assult', 'Spirit Refuge', 'Shadow Dash', 'Stand United', 'Bruiser'),
-('Singed', 'TOP', 'Noxious Slipstream', 'Poison Trail', 'Mega Adhesive', 'Fling', 'Insanity Potion', 'Tank'),
-('Taric', 'SUP', 'Bravado', 'Starlight Touch', 'Bastion', 'Dazzle', 'Cosmic Radiance', 'Tank'),
-('Teemo', 'TOP', 'Guerrilla Warfare', 'Blinding Dart', 'Move Quick', 'Toxic Shot', 'Noxious Trap', 'Mage'),
-('Thresh', 'SUP', 'Damnation', 'Death Sentence', 'Dark Passage', 'Flay', 'The Box', 'Fighter'),
-('Tristana', 'ADC', 'Draw a Bead', 'Rapid Fire', 'Rocket Jump', 'Explosive Charge', 'Buster Shot', 'Ranged Carry'),
-('Twisted Fate', 'MID', 'Loaded Dice', 'Wild Cards', 'Pick a Card', 'Stacked Deck', 'Destiny', 'Mage'),
-('Udyr', 'JNG', 'Monkey Agility', 'Tiger Stance', 'Turtle Stance', 'Bear Stance', 'Phoenix Stance', 'Bruiser'),
-('Vladimir', 'MID', 'Crimson Pact', 'Transfusion', 'Sanguine Pool', 'Tides of Blood', 'Hemoplague', 'Mage'),
-('Volibear', 'JNG', 'Chosen of the Storm', 'Rolling Thunder', 'Frenzy', 'Majestic Roar', 'Thunder Claws', 'Bruiser'),
-('Zed', 'MID', 'Condempt Weak', 'Razor Shuriken', 'Living Shadow', 'Shadow Slash', 'Death Mark', 'Assassin');
+INSERT INTO champion VALUES ('Vayne', 'ADC', 'Night Hunter', 'Tumble', 'Silver Bolts', 'Condemn', 'Final Hour', 'Hyper Carry');
+INSERT INTO champion VALUES ('Aatrox', 'TOP', 'Deathbringer', 'Darkin Dlade', 'Infernal Chains', 'Umbral Dash', 'World Ender', 'Bruiser');
+INSERT INTO champion VALUES ('Ahri', 'MID', 'Vastayan Grace', 'Orb of Deception', 'Fox-Fire', 'Charm', 'Spirit Rush', 'Mage');
+INSERT INTO champion VALUES ('Akali', 'MID', 'Assassin Mark', 'Five Point Strike', 'Twilight Shroud', 'Shuriken Flip', 'Perfect Execution', 'Assassin');
+INSERT INTO champion VALUES ('Alistar', 'SUP', 'Triumphant Roar', 'Pulverize', 'Headbutt', 'Trample', 'Unbreakable Will', 'Tank');
+INSERT INTO champion VALUES ('Ashe', 'ADC', 'Frost Shot', 'Ramper Focus', 'Volley', 'Hawkshot', 'Enchanted Arrow', 'Ranged Carry');
+INSERT INTO champion VALUES ('Azir', 'MID', 'Shurima Legacy', 'Conquering Sands', 'Arise', 'Shifting Sands', 'Emperor Divide', 'Mage');
+INSERT INTO champion VALUES ('Bard', 'SUP', 'Traveler Call', 'Cosmic Binding', 'Caretaker Shrine', 'Magical Journey', 'Tempered Fate', 'Mage');
+INSERT INTO champion VALUES ('Blitz', 'SUP', 'Mana Barrier', 'Rocket Grab', 'Overdrive', 'Power Fist', 'Static Field', 'Bruiser');
+INSERT INTO champion VALUES ('Braum', 'SUP', 'Concussive Blows', 'Winter Bite', 'Stand Behind Me', 'Unbreakable', 'Glacial Fissure', 'Tank');
+INSERT INTO champion VALUES ('Caitlyn', 'ADC', 'Headshot', 'Peacemaker', 'Yordle Snap Trap', '90 Caliber net', 'Ace in the Hole', 'Ranged Carry');
+INSERT INTO champion VALUES ('Camile', 'TOP', 'Adaptive Defense', 'Precision Protocol', 'Tatical Sweep', 'Hookshot', 'Hextech Ultimatum', 'Bruiser');
+INSERT INTO champion VALUES ('Cassiopeia', 'MID', 'Serpentine Grace', 'Noxious Blast', 'Miasma', 'Twin Fang', 'Petrifying Gaze', 'Mage');
+INSERT INTO champion VALUES ('Diana', 'MID', 'Moonsilver Blade', 'Crescent Strike', 'Pale Cascade', 'Moonfall', 'Lunar Rush', 'Assassin');
+INSERT INTO champion VALUES ('Dr. Mundo', 'TOP', 'Adrenaline Rush', 'Infected Cleaver', 'Burning Agony', 'Masochism', 'Sadism', 'Tank');
+INSERT INTO champion VALUES ('Draven', 'ADC', 'League of Draven', 'Spinning Axe', 'Blood Rush', 'Stand Aside', 'Whirling Death', 'Ranged Carry');
+INSERT INTO champion VALUES ('Ekko', 'JNG', 'Z-Drive', 'Timewinder', 'Parallel Convergence', 'Phase Dive', 'Chronobreak', 'Mage');
+INSERT INTO champion VALUES ('Elise', 'JNG', 'Spider Queen', 'Neurotoxin', 'Volatile Spiderling', 'Cacoon', 'Spider Form', 'Assassin');
+INSERT INTO champion VALUES ('Ezreal', 'ADC', 'Rising Spell Force', 'Mystic Shot', 'Essence Flux', 'Arcane Shift', 'Trueshot Barrage', 'Ranged Carry');
+INSERT INTO champion VALUES ('Fiora', 'TOP', 'Duelist Dance', 'Lunge', 'Riposte', 'Bladework', 'Grand Challenge', 'Bruiser');
+INSERT INTO champion VALUES ('Fizz', 'MID', 'Nimble Fighter', 'Urchin Strike', 'Seastone Trident', 'Trickster', 'Chum the Waters', 'Assassin');
+INSERT INTO champion VALUES ('Galio', 'MID', 'Colossal Smash', 'Winds of War', 'Shield of Durand', 'Justice Punch', 'Hero Entrance', 'Tank');
+INSERT INTO champion VALUES ('Gangplank', 'TOP', 'Trial by Fire', 'Parrley', 'Remove Scurvy', 'Powder Keg', 'Cannon Barrage', 'Fighter');
+INSERT INTO champion VALUES ('Garen', 'TOP', 'Perseverance', 'Decisive Strike', 'Courage', 'Judgment', 'Demacian Justice', 'Bruiser');
+INSERT INTO champion VALUES ('Gragas', 'JNG', 'Happy Hour', 'Barrel Roll', 'Druken Rage', 'Body Slam', 'Explosive Cask', 'Bruiser');
+INSERT INTO champion VALUES ('Graves', 'JNG', 'New Destiny', 'End of the Line', 'Smoke Screen', 'Quickdraw', 'Collateral Damage', 'Fighter');
+INSERT INTO champion VALUES ('Hecarim', 'JNG', 'Warpath', 'Rampage', 'Spirit of Dread', 'Devastating Charge', 'Onslaught of Shadows', 'Bruiser');
+INSERT INTO champion VALUES ('Irelia', 'TOP', 'Ionian Fervor', 'Bladesurge', 'Defiant Dance', 'Flawless Duet', 'Vanguard Edge', 'Fighter');
+INSERT INTO champion VALUES ('Ivern', 'JNG', 'Friend the Forest', 'Rootcaller', 'Brushmaker', 'Triggerseed', 'Daisy!', 'Mage');
+INSERT INTO champion VALUES ('Janna', 'SUP', 'Tailwind', 'Howling Gale', 'Zephyr', 'Eye Of The Storm', 'Monsoon', 'Mage');
+INSERT INTO champion VALUES ('Jarvan IV', 'JNG', 'Martial Cadence', 'Dragon Strike', 'Golden Aegis', 'Demacian Standard', 'Cataclysm', 'Bruiser');
+INSERT INTO champion VALUES ('Jax', 'TOP', 'Relentless Atk', 'Leap Strike', 'Empower', 'Counter Strike', 'Grand Might', 'Bruiser');
+INSERT INTO champion VALUES ('Jayce', 'MID', 'Hextech Capacitor', 'Shock Blast', 'Hyper Charge', 'Acceleration Gate', 'Mecury Cannon', 'Fighter');
+INSERT INTO champion VALUES ('Jhin', 'ADC', 'Whisper', 'Dancing Grenade', 'Deadly Flourish', 'Captive Audience', 'Curtain Call', 'Ranged Carry');
+INSERT INTO champion VALUES ('Jinx', 'ADC', 'Get Excited!', 'Switcheroo!', 'Zap!', 'Flame Chompers!', 'Super Mega Rocket!', 'Hyper Carry');
+INSERT INTO champion VALUES ('Kaisa', 'ADC', 'Second Skin', 'Icathian Rain', 'Void Seeker', 'Supercharge', 'Killer Instinct', 'Ranged Carry');
+INSERT INTO champion VALUES ('Kalista', 'ADC', 'Martial Poise', 'Pierce', 'Sentinel', 'Rend', 'Fate Call', 'Ranged Carry');
+INSERT INTO champion VALUES ('Karma', 'SUP', 'Gathering Fire', 'Inner Flame', 'Focused Resolve', 'Inspire', 'Mantra', 'Mage');
+INSERT INTO champion VALUES ('Kayn', 'JNG', 'Darkin Scythe', 'Reaping Slash', 'Blade Reach', 'Shadow Step', 'Umbral Trespass', 'Bruiser');
+INSERT INTO champion VALUES ('Khazix', 'JNG', 'Unseen Threat', 'Taste Their Fear', 'Void Spike', 'Leap', 'Void Assult', 'Assassin');
+INSERT INTO champion VALUES ('Kled', 'TOP', 'Skaarl, the Lizard', 'Bear Trap', 'Violent Tendencies', 'Jousting', 'Chaaaaarge!!!', 'Bruiser');
+INSERT INTO champion VALUES ('Lee Sin', 'JNG', 'Flurry', 'Resonating Strike', 'Safeguard', 'Tempest', 'Dragon Rage', 'Fighter');
+INSERT INTO champion VALUES ('Lucian', 'ADC', 'Lightslinger', 'Piercing Light', 'Ardent Blast', 'Relentless Pursuit', 'The Culling', 'Ranged Carry');
+INSERT INTO champion VALUES ('Lulu', 'SUP', 'Pix Faerie', 'Glitterlance', 'Whimsy', 'Help, Pix!', 'Wild Growth', 'Mage');
+INSERT INTO champion VALUES ('Nami', 'SUP', 'Surging Tides', 'Aqua Prison', 'Ebb and Flow', 'Tidecaller Blessing', 'Tidal Wave', 'Mage');
+INSERT INTO champion VALUES ('Nautilus', 'SUP', 'Staggering Blow', 'Dredge Line', 'Titan Wrath', 'Riptide', 'Depth Charge', 'Tank');
+INSERT INTO champion VALUES ('Nidalee', 'JNG', 'Prowl', 'Javelin Toss', 'Bushwhack', 'Swipe', 'Transform', 'Assassin');
+INSERT INTO champion VALUES ('Olaf', 'JNG', 'Berserker Rage', 'Undertow', 'Vicious Strikes', 'Reckless Swing', 'Ragnarok', 'Bruiser');
+INSERT INTO champion VALUES ('Oriana', 'MID', 'Clockwork Windup', 'CMD:Attack', 'CMD:Dissonance', 'CMD:Protect', 'CMD:Shockwave', 'Mage');
+INSERT INTO champion VALUES ('Ornn', 'TOP', 'Living Forge', 'Volcanic Rupture', 'Bellows Breath', 'Searing Charge', 'Call of Forge God', 'Tank');
+INSERT INTO champion VALUES ('Reksai', 'JNG', 'Fury of the XerSai', 'Prey Seeker', 'Burrow', 'Tunnel', 'Void Rush', 'Bruiser');
+INSERT INTO champion VALUES ('Rengar', 'JNG', 'Unseen Predator', 'Savager', 'Battle Roar', 'Bola Strike', 'Thrill of the Hunt', 'Assassin');
+INSERT INTO champion VALUES ('Shen', 'TOP', 'Ki Barrier', 'Twilight Assult', 'Spirit Refuge', 'Shadow Dash', 'Stand United', 'Bruiser');
+INSERT INTO champion VALUES ('Singed', 'TOP', 'Noxious Slipstream', 'Poison Trail', 'Mega Adhesive', 'Fling', 'Insanity Potion', 'Tank');
+INSERT INTO champion VALUES ('Taric', 'SUP', 'Bravado', 'Starlight Touch', 'Bastion', 'Dazzle', 'Cosmic Radiance', 'Tank');
+INSERT INTO champion VALUES ('Teemo', 'TOP', 'Guerrilla Warfare', 'Blinding Dart', 'Move Quick', 'Toxic Shot', 'Noxious Trap', 'Mage');
+INSERT INTO champion VALUES ('Thresh', 'SUP', 'Damnation', 'Death Sentence', 'Dark Passage', 'Flay', 'The Box', 'Fighter');
+INSERT INTO champion VALUES ('Tristana', 'ADC', 'Draw a Bead', 'Rapid Fire', 'Rocket Jump', 'Explosive Charge', 'Buster Shot', 'Ranged Carry');
+INSERT INTO champion VALUES ('Twisted Fate', 'MID', 'Loaded Dice', 'Wild Cards', 'Pick a Card', 'Stacked Deck', 'Destiny', 'Mage');
+INSERT INTO champion VALUES ('Udyr', 'JNG', 'Monkey Agility', 'Tiger Stance', 'Turtle Stance', 'Bear Stance', 'Phoenix Stance', 'Bruiser');
+INSERT INTO champion VALUES ('Vladimir', 'MID', 'Crimson Pact', 'Transfusion', 'Sanguine Pool', 'Tides of Blood', 'Hemoplague', 'Mage');
+INSERT INTO champion VALUES ('Volibear', 'JNG', 'Chosen of the Storm', 'Rolling Thunder', 'Frenzy', 'Majestic Roar', 'Thunder Claws', 'Bruiser');
+INSERT INTO champion VALUES ('Zed', 'MID', 'Condempt Weak', 'Razor Shuriken', 'Living Shadow', 'Shadow Slash', 'Death Mark', 'Assassin');

95875d651bcd3a51d4e815657a6901efecea00b7
Wesley Ryder <wr140815@ohio.edu>
2019-11-13 01:33:40 -0500

    biggg commit aka thicccc

---
+++ insert_riot.sql
@@ -0,0 +1,3 @@
+INSERT INTO riot_reward VALUES ('999888777', '001', 'Disable Chat', '08212019');
+INSERT INTO riot_reward VALUES ('999888666', '002', 'Faker', '09022019');
+INSERT INTO riot_reward VALUES ('999888555', '003', 'Penut', '10202019');

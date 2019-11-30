2dc0fc8c5f32c17c67deff9202082e4abadbe19b
Wesley Ryder <wr140815@ohio.edu>
2019-11-30 05:09:06 -0500

    removed main dir scripts

--- insert_rune_page.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO rune_page VALUES ('123456789', 'Hola', 'Precision', 'Domination', 'Disable Chat');
-INSERT INTO rune_page VALUES ('223456789', 'Gogogo', 'Sorcery', 'Resolve', 'Disable Chat');
-INSERT INTO rune_page VALUES ('323456789', 'GG', 'Precision', 'Resolve', 'Faker');
-INSERT INTO rune_page VALUES ('423456789', 'Uh', 'Precision', 'Sorcery', 'Micah');
-INSERT INTO rune_page VALUES ('523456789', 'Same', 'Sorcery', 'Precision', 'Faker');
-INSERT INTO rune_page VALUES ('663456789', 'Something', 'Resolve', 'Precision', 'Disable Chat');
-INSERT INTO rune_page VALUES ('773456789', 'My rune', 'Resolve', 'Domination', 'Teddy');
-INSERT INTO rune_page VALUES ('923456789', 'Lol', 'Precision', 'Domination', 'Micah');
-INSERT INTO rune_page VALUES ('123123123', 'Ripironi', 'Precision', 'Domination', 'Slazlon');

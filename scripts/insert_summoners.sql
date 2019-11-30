7e0e3a866a6a65347ac5f5372a66b3279dfb9a79
Wesley Ryder <wr140815@ohio.edu>
2019-11-30 05:04:14 -0500

    moved but seeing if they were updated remotely

---
+++ scripts/insert_summoners.sql
@@ -0,0 +1,12 @@
+INSERT INTO summoner VALUES ('Disable Chat',    3000, 'Challenger', 5, 405, 254, 174);
+INSERT INTO summoner VALUES ('Slazlon',         1710, 'Gold 2', 4, 204, 843, 849);
+INSERT INTO summoner VALUES ('Micah',           2400, 'Diamond 1', 4, 300, 502, 498);
+INSERT INTO summoner VALUES ('Gosu',            2400, 'Diamond 1', 3, 183, 754, 698);
+INSERT INTO summoner VALUES ('Faker',           3000, 'Challenger', 5, 509, 200, 143);
+INSERT INTO summoner VALUES ('Teddy',           2600, 'Master', 5, 338, 120, 60);
+INSERT INTO summoner VALUES ('Penut',           3000, 'Challenger', 5, 405, 100, 80);
+INSERT INTO summoner VALUES ('RagingMonkey',    1500, 'Gold 5', 2, 123, 1024, 1048);
+INSERT INTO summoner VALUES ('Bacon',           1640, 'Gold 3', 1, 365, 428, 421);
+INSERT INTO summoner VALUES ('Rip',             1710, 'Gold 2', 0, 100, 210, 231);
+INSERT INTO summoner VALUES ('Same',            1290, 'Silver 3', 4, 312, 831, 890);
+INSERT INTO summoner VALUES ('Bad Player',      1150, 'Silver 5', 5, 256, 1121, 1200);
